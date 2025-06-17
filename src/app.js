ff6b3886ca71f20a495f0c714601d814cd85787e
Jeff <jeff@gmail.com>
2025-06-17 11:29:00 -0500

    upload project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+//para configurar el servidor
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//configuracion
+app.set('port', process.env.PORT || 4000)
+
+//middlewares --> logica, funcionalidades para primero validar antes de llegar a las rutas
+app.use(cors())
+app.use(express.json()) //envia y recibe en json
+
+
+//rutas
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi api restfull')
+})
+
+
+
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// para el arranque de la aplicacion
+require('dotenv').config()
+
+const app = require('./app')
+require('./database')
+
+//esta logica es para ejecutar el servidor
+async function main() {
+    await app.listen(app.get('port'))
+    console.log('el servidor se esta ejecutando en el puerto', app.get('port'));
+}
+
+main();
